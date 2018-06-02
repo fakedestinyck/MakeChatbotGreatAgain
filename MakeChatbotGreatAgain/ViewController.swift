2fd0f68b84c7f3e68b647719a97171f9971765dd
Haochang Qian <fakedestinyck@gmail.com>
2018-06-02 12:13:08 +0800

    Initial Commit

---
+++ MakeChatbotGreatAgain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeChatbotGreatAgain
+//
+//  Created by  on Jun/2/2018.
+//  Copyright © 2018 HACKxSJTU2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

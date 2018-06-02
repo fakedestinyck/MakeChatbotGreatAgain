50be5a23dc0adc596402a1d4814c86ecbb05e592
Haochang Qian <fakedestinyck@gmail.com>
2018-06-02 18:06:23 +0800

    add basics

--- MakeChatbotGreatAgain/ViewController.swift
+++ MakeChatbotGreatAgain/ViewController.swift
@@ -10,19 +10,233 @@ import UIKit
 import AVFoundation
 import AssistantV1
 import SpeechToTextV1
+import JSQMessagesViewController
 
-class ViewController: UIViewController {
-
+class ViewController: JSQMessagesViewController {
+    
+    var messages = [JSQMessage]()
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    
+    var assistant: Assistant!
+    var speechToText: SpeechToText!
+    
+    var audioPlayer: AVAudioPlayer?
+    var workspace = Credentials.AssistantWorkspace
+    var context: Context?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupInterface()
+        setupSender()
+        setupWatsonServices()
+        startAssistant()
     }
+    
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// MARK: Watson Services
+extension ViewController {
+    
+    /// Instantiate the Watson services
+    func setupWatsonServices() {
+        assistant = Assistant(
+            username: Credentials.AssistantUsername,
+            password: Credentials.AssistantPassword,
+            version: "2017-05-26"
+        )
+        speechToText = SpeechToText(
+            username: Credentials.SpeechToTextUsername,
+            password: Credentials.SpeechToTextPassword
+        )
     }
+    
+    /// Present an error message
+    func failure(error: Error) {
+        let alert = UIAlertController(
+            title: "Watson Error",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+    
+    /// Start a new conversation
+    func startAssistant() {
+        assistant.message(
+            workspaceID: workspace,
+            failure: failure,
+            success: presentResponse
+        )
+    }
+    
+    /// Present a conversation reply and speak it to the user
+    func presentResponse(_ response: MessageResponse) {
+        let text = response.output.text.joined()
+        context = response.context // save context to continue conversation
+        
+//        // synthesize and speak the response
+//        textToSpeech.synthesize(text: text, accept: "audio/wav", failure: failure) { audio in
+//            self.audioPlayer = try? AVAudioPlayer(data: audio)
+//            self.audioPlayer?.prepareToPlay()
+//            self.audioPlayer?.play()
+//        }
+        
+        // create message
+        let message = JSQMessage(
+            senderId: User.watson.rawValue,
+            displayName: User.getName(User.watson),
+            text: text
+        )
+        
+        // add message to chat window
+        if let message = message {
+            self.messages.append(message)
+            DispatchQueue.main.async { self.finishSendingMessage() }
+        }
+    }
+    
+    /// Start transcribing microphone audio
+    @objc func startTranscribing() {
+        audioPlayer?.stop()
+        var settings = RecognitionSettings(contentType: ".opus")
+        settings.interimResults = true
+        speechToText.recognizeMicrophone(settings: settings, failure: failure) { results in
+            var accumulator = SpeechRecognitionResultsAccumulator()
+            accumulator.add(results: results)
+            print(accumulator.bestTranscript)
+            
+            self.inputToolbar.contentView.textView.text = accumulator.bestTranscript
+            self.inputToolbar.toggleSendButtonEnabled()
+        }
+    }
+    
+    /// Stop transcribing microphone audio
+    @objc func stopTranscribing() {
+        speechToText.stopRecognizeMicrophone()
+    }
+}
 
+// MARK: Configuration
+extension ViewController {
+    
+    func setupInterface() {
+        // bubbles
+        let factory = JSQMessagesBubbleImageFactory()
+        let incomingColor = UIColor.jsq_messageBubbleLightGray()
+        let outgoingColor = UIColor.jsq_messageBubbleGreen()
+        incomingBubble = factory!.incomingMessagesBubbleImage(with: incomingColor)
+        outgoingBubble = factory!.outgoingMessagesBubbleImage(with: outgoingColor)
+        
+        // avatars
+        collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
+        // microphone button
+        let microphoneButton = UIButton(type: .custom)
+        microphoneButton.setImage(#imageLiteral(resourceName: "microphone-hollow"), for: .normal)
+        microphoneButton.setImage(#imageLiteral(resourceName: "microphone"), for: .highlighted)
+        microphoneButton.addTarget(self, action: #selector(startTranscribing), for: .touchDown)
+        microphoneButton.addTarget(self, action: #selector(stopTranscribing), for: .touchUpInside)
+        microphoneButton.addTarget(self, action: #selector(stopTranscribing), for: .touchUpOutside)
+        inputToolbar.contentView.leftBarButtonItem = microphoneButton
+    }
+    
+    func setupSender() {
+        senderId = User.me.rawValue
+        senderDisplayName = User.getName(User.me)
+    }
+    
+    override func didPressSend(
+        _ button: UIButton!,
+        withMessageText text: String!,
+        senderId: String!,
+        senderDisplayName: String!,
+        date: Date!)
+    {
+        let message = JSQMessage(
+            senderId: User.me.rawValue,
+            senderDisplayName: User.getName(User.me),
+            date: date,
+            text: text
+        )
+        
+        if let message = message {
+            self.messages.append(message)
+            self.finishSendingMessage(animated: true)
+        }
+        
+        // send text to conversation service
+        let input = InputData(text: text)
+        let request = MessageRequest(input: input, context: context)
+        assistant.message(
+            workspaceID: workspace,
+            request: request,
+            failure: failure,
+            success: presentResponse
+        )
+    }
+    
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        // required by super class
+    }
+}
 
+// MARK: Collection View Data Source
+extension ViewController {
+    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int)
+        -> Int
+    {
+        return messages.count
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        messageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageData!
+    {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        messageBubbleImageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageBubbleImageDataSource!
+    {
+        let message = messages[indexPath.item]
+        let isOutgoing = (message.senderId == senderId)
+        let bubble = (isOutgoing) ? outgoingBubble : incomingBubble
+        return bubble
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        avatarImageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageAvatarImageDataSource!
+    {
+        let message = messages[indexPath.item]
+        return User.getAvatar(message.senderId)
+    }
+    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath)
+        -> UICollectionViewCell
+    {
+        let cell = super.collectionView(
+            collectionView,
+            cellForItemAt: indexPath
+        )
+        let jsqCell = cell as! JSQMessagesCollectionViewCell
+        let message = messages[indexPath.item]
+        let isOutgoing = (message.senderId == senderId)
+        jsqCell.textView.textColor = (isOutgoing) ? .white : .black
+        return jsqCell
+    }
 }
 
